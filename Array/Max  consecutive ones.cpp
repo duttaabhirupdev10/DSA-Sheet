ee89bb70e3b0ce52f9951fa300d8909556a52b46
Abhirup Dutta <duttaabhirup241@gmail.com>
2026-02-13 10:27:16 +0530

    Refactor findMaxConsecutiveOnes to Java syntax

--- Array/Max  consecutive ones.cpp
+++ Array/Max  consecutive ones.cpp
@@ -1,18 +1,18 @@
 // only optimal. no brute better
 class Solution {
-public:
-    int findMaxConsecutiveOnes(vector<int>& nums) {
-        int maxi=0;
-        int cnt=0;
-        for( int i=0; i< nums.size() ; i++){
-            if (nums[i] == 1){
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxi = 0;
+        int cnt = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
                 cnt++;
-                maxi=max(maxi,cnt);
-            }
-            else{
-                cnt=0;
+                maxi = Math.max(maxi, cnt);
+            } else {
+                cnt = 0;
             }
         }
+
         return maxi;
     }
-};
+}
