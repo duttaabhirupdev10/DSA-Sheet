80de3fabe4720537747cf45dffe0552094fb262c
Abhirup Dutta <duttaabhirup241@gmail.com>
2026-02-13 10:36:26 +0530

    Update and rename largestelementinarray.cpp to largestElemenIinArray.java

---
+++ Array/largestElemenIinArray.java
@@ -0,0 +1,14 @@
+// Largest element in the array
+class Solution {
+    public int largest(int[] arr) {
+        int largest = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        return largest;
+    }
+}
