31c383fd9c4be039e817e737f236101961ec482f
Abhirup Dutta <duttaabhirup241@gmail.com>
2026-04-23 23:16:51 +0530

    Add leaders finding method in LeadersInArray class
    Implement a method to find leaders in an array, where a leader is greater than all elements to its right. The method returns a list of leaders in their original order.

---
+++ Array/LeadersInArray.java
@@ -0,0 +1,36 @@
+/* Given an integer array nums, return a list of all the leaders in the array.
+
+A leader in an array is an element whose value is strictly greater than all elements to its right in the given array. 
+The rightmost element is always a leader. 
+The elements in the leader array must appear in the order they appear in the nums array.
+
+Example 1
+
+Input: nums = [1, 2, 5, 3, 1, 2]
+
+Output: [5, 3, 2]
+
+Explanation:
+
+2 is the rightmost element, 3 is the largest element in the index range [3, 5], 5 is the largest element in the index range [2, 5] */
+
+class Solution {
+    public List<Integer> leaders(int[] nums) {
+        List<Integer> result=new ArrayList<>();
+        // store the min number in the maxRight
+        int maxRight=Integer.MIN_VALUE;
+        //traverse the array from the right to the left side
+        for(int i=nums.length-1;i>=0;i--){
+            if(nums[i]> maxRight){
+                result.add(nums[i]);
+                maxRight=nums[i];
+            }
+        }
+        //reverse to maintain original order
+        Collections.reverse(result);
+        return result;
+    }
+}
+
+//Time Complexity--->O(n)
+//Space Complexity--->O(n)   for result list
