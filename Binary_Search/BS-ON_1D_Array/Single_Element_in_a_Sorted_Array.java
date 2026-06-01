bd3e800765fb92048672a4c78101ac47f44a6c18
Abhirup Dutta <duttaabhirup241@gmail.com>
2026-06-01 23:44:46 +0530

    Create Single_Element_in_a_Sorted_Array.java

---
+++ Binary_Search/BS-ON_1D_Array/Single_Element_in_a_Sorted_Array.java
@@ -0,0 +1,41 @@
+/*
+You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+
+Return the single element that appears only once.
+
+Your solution must run in O(log n) time and O(1) space.
+
+Example 1:
+Input: nums = [1,1,2,3,3,4,4,8,8]
+Output: 2
+
+Example 2:
+Input: nums = [3,3,7,7,10,11,11]
+Output: 10
+*/
+
+// brute
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+
+        if (n == 1) return nums[0];
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                if (nums[i] != nums[i + 1])
+                    return nums[i];
+            } else if (i == n - 1) {
+                if (nums[i] != nums[i - 1])
+                    return nums[i];
+            } else {
+                if (nums[i] != nums[i - 1] && nums[i] != nums[i + 1])
+                    return nums[i];
+            }
+        }
+
+        return -1;
+    }
+}  // TC--->O(N)
+
+// Optimal
