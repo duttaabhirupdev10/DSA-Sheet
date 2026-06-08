43235b13c26bf56f66eb9d34620f5cf2ba5312e9
Abhirup Dutta <duttaabhirup241@gmail.com>
2026-06-08 20:27:40 +0530

    Add binary search method for calculating square root
    Implemented a method to find the floor value of the square root of a positive integer using binary search.

--- Binary_Search/BS-ON_Answer/Finding Sqrt of a number using Binary Search.java
+++ Binary_Search/BS-ON_Answer/Finding Sqrt of a number using Binary Search.java
@@ -1,1 +1,26 @@
+/*
+Given a positive integer n. Find and return its square root. If n is not a perfect square, then return the floor value of sqrt(n).
 
+
+Example 1:
+Input: n = 36
+Output: 6
+Explanation: 6 is the square root of 36.
+*/
+// optimal
+class Solution {
+    public int floorSqrt(int n) {
+     int low=1,high=n , ans=1;
+     while(low <= high){
+       int mid=(low+high)/2;
+        if((mid*mid) <= n){
+            ans=mid;
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+     }
+     return ans;
+    }
+}    //TC---> O(log n)
