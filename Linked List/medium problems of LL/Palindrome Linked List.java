67f1d4a10780fa98b75a074782b977471a99fc1a
Abhirup Dutta <duttaabhirup241@gmail.com>
2026-07-21 12:47:51 +0530

    Add optimal palindrome check for linked list
    Implemented an optimal solution to check if a linked list is a palindrome using two-pointer technique and reversing the second half of the list.

--- Linked List/medium problems of LL/Palindrome Linked List.java
+++ Linked List/medium problems of LL/Palindrome Linked List.java
@@ -37,4 +37,51 @@ class Solution {
 }      //  TC-->O(2N)    sc-->o(N)
 
 
-// optimal
+// OPTIMAL
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast.next != null && fast.next.next != null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode newHead=reverse(slow.next);
+        ListNode first=head;
+        ListNode second=newHead;
+        while(second != null){
+            if(first.val != second.val){
+               // return(newHead);
+                return false;
+            }
+            first=first.next;
+            second=second.next;
+        }
+        reverse(newHead);
+        return true;
+    }
+    // revese function
+    private ListNode reverse(ListNode head) {
+        ListNode temp=head;
+        ListNode prev=null;
+        while(temp != null){
+            ListNode front=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=front;
+        }
+        return prev;
+    }
+}
+// TC -->O(2N)   SC-->O(1)
