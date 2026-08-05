851b244ef9c50797fc09f266b4d67b5c68c5abb6
Abhirup Dutta <duttaabhirup241@gmail.com>
2026-08-05 11:20:21 +0530

    Fix formatting and comments in MyStack implementation

--- Stack and Queues/learning/225. Implement Stack using Queues.java
+++ Stack and Queues/learning/225. Implement Stack using Queues.java
@@ -28,3 +28,38 @@ myStack.top(); // return 2
 myStack.pop(); // return 2
 myStack.empty(); // return False
 */
+
+class MyStack {
+    private Queue<Integer>queue;
+    public MyStack() {
+        queue=new LinkedList<>();
+    }
+    
+    public void push(int x) {
+    queue.add(x);
+    for(int i=1;i<queue.size();i++){
+        queue.add(queue.remove());
+    }        
+    }
+    
+    public int pop() {
+        return queue.remove();
+    }
+    
+    public int top() {
+        return queue.peek();
+    }
+    
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}///tc-->O(n)  , sc--->o(n)
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
