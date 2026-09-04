ae93ae30260151a7f70d882074737076dbe37cf1
Abhirup Dutta <duttaabhirup241@gmail.com>
2026-09-04 12:02:38 +0530

    Add optimal solution for longest substring problem
    Implemented an optimal solution for finding the length of the longest substring without repeating characters using a sliding window approach.

--- Sliding Window & 2 pointer/Medium/Longest Substring Without Repeating Characters.java
+++ Sliding Window & 2 pointer/Medium/Longest Substring Without Repeating Characters.java
@@ -36,3 +36,35 @@ class Solution {
         return res;
     }
 }    //   TC--->O(N^2)   ,    SC--->O(1)
+
+
+// optimal
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        
+        int n = s.length();
+
+        int[] hash = new int[256];
+        Arrays.fill(hash, -1);
+
+        int l = 0;
+        int r = 0;
+        int maxLen = 0;
+
+        while (r < n) {
+
+            if (hash[s.charAt(r)] != -1) {
+                l = Math.max(hash[s.charAt(r)] + 1, l);
+            }
+
+            int len = r - l + 1;
+            maxLen = Math.max(maxLen, len);
+
+            hash[s.charAt(r)] = r;
+
+            r++;
+        }
+
+        return maxLen;
+    }
+}         //    TC--->O(N)   ,     SC--->O(N)
